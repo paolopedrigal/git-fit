b497c339689c8624d18336597f8c3b02697a2a06
Paolo Pedrigal <paolopedrigal@gmail.com>
2024-08-01 02:41:22 -0700

    fix: pydantic models for user and token, update typing

--- backend/auth.py
+++ backend/auth.py
@@ -7,6 +7,7 @@ from passlib.context import CryptContext
 from pydantic import BaseModel
 import jwt
 from jwt.exceptions import InvalidTokenError
+from uuid import UUID
 
 from backend.database import SessionLocal
 from . import TOKEN_TYPE, dotenv, crud, schemas
@@ -30,10 +31,9 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-class TokenUserData(BaseModel):
-    token: Token
+class TokenUserData(Token):
     username: str
-    id: int
+    id: UUID
 
 # Dependency
 def get_db():
@@ -53,8 +53,6 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
-# def get_payload()
-
 def authenticate_user(db: Session, username: str, password: str):
     user = crud.read_user_by_username(db, username)
     if not user:
@@ -63,9 +61,9 @@ def authenticate_user(db: Session, username: str, password: str):
         return False
     return user
 
-# def create_access_token(data: dict, expires_delta: timedelta | None = None):
-def create_access_token(username: str, user_id: int, expires_delta: timedelta | None = None):
-    to_encode = {"sub": username, "id": user_id}
+def create_access_token(username: str, user_id: UUID, expires_delta: timedelta | None = None):
+    encoded_uuid_id = str(user_id) # convert uuid to string to make it JSON serializable
+    to_encode = {"sub": username, "id": encoded_uuid_id}
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
@@ -97,14 +95,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)], db: Se
 
 # Create user
 @router.post("/", response_model=TokenUserData)
-def post_user(user: schemas.UserBase, db: Session = Depends(get_db)) -> Any:
+def post_user(user: schemas.UserCreate, db: Session = Depends(get_db)) -> Any:
     check_user = crud.read_user_by_username(db, username=user.username)
     if check_user: # if user already exists
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="User already registered")
     new_user = crud.create_user(db=db, user=user)
     access_token=create_access_token(new_user.username, new_user.id)
-    return TokenUserData(username=new_user.username, id=new_user.id, token=Token(access_token=access_token, token_type=TOKEN_TYPE))
-
+    return TokenUserData(username=new_user.username, id=new_user.id, access_token=access_token, token_type=TOKEN_TYPE)
+    
 # Create token for login
 @router.post("/token", response_model=TokenUserData)
 def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
@@ -116,4 +114,4 @@ def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depen
             headers={"WWW-Authenticate": "Bearer"}
         )
     access_token=create_access_token(user.username, user.id)
-    return TokenUserData(username=user.username, id=user.id, token=Token(access_token=access_token, token_type=TOKEN_TYPE))
+    return TokenUserData(username=user.username, id=user.id, access_token=access_token, token_type=TOKEN_TYPE)

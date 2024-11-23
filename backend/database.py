75bcd6ad1b33d31e036886c50554ddc9157c9110
Paolo Pedrigal <paolopedrigal@gmail.com>
2024-11-23 14:20:23 -0800

    docker: containerized backend

--- backend/database.py
+++ backend/database.py
@@ -6,12 +6,14 @@ from backend.config import config
 # Check for missing keys from .env and handle them
 POSTGRES_USER = config.get("POSTGRES_USER")
 POSTGRES_PASSWORD = config.get("POSTGRES_PASSWORD")
-POSTGRES_SERVER= config.get("POSTGRES_SERVER")
+POSTGRES_SERVER = config.get("POSTGRES_SERVER")
+POSTGRES_DB = config.get("POSTGRES_DB")  # Get the database name from .env
 
-if not POSTGRES_USER or not POSTGRES_PASSWORD or not POSTGRES_SERVER:
-    raise KeyError("Missing one or more environment variables: POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PATH")
+if not POSTGRES_USER or not POSTGRES_PASSWORD or not POSTGRES_SERVER or not POSTGRES_DB:
+    raise KeyError("Missing one or more environment variables: POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_DB")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}"
+# Correct the database URL to include the database name
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)

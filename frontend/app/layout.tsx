1f48b6e7a3191a75c1f798209314f166e51d6748
Paolo Pedrigal <paolopedrigal@gmail.com>
2024-12-16 17:19:40 -0800

    feat: favicons for web + mobile

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,6 +10,36 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Git Fit",
   description: "Workout Tracker for Programmers",
+  icons: {
+    icon: "/favicon.ico", // Default favicon
+    apple: "/apple-touch-icon.png", // iOS icon
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
